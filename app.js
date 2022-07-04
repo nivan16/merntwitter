dd86fafd64cf7cf4ab5a4404ad6755bddd220b9f
App Academy Student <appacademy@apps-mac-mini-6.myfiosgateway.com>
2022-07-04 05:57:45 -0400

    done

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5001;
 const bodyParser = require('body-parser');
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
-
+const path = require('path');
 const passport = require('passport');
 app.use(passport.initialize());
 require('./config/passport')(passport);
@@ -16,7 +16,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.listen(port, ()=> console.log(`Server is running on port ${port}`))
 
-
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
